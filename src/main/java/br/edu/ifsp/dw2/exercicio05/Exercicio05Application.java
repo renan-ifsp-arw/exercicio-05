0f289ca9950c96692db4f0678a30662431ad5b57
braga-apixio <rbraga@apixio.com>
2023-08-24 10:57:27 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifsp/dw2/exercicio05/Exercicio05Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.dw2.exercicio05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercicio05Application.class, args);
+    }
+
+}
